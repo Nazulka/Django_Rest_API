19b327c791d1d33904de70e3307422d9f4a9fb9b
Nazulka <nazeekadiu@yahoo.co.uk>
2021-09-08 22:48:55 +0000

    Add address model, migrate

--- api/address_book/models.py
+++ api/address_book/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    street = models.CharField(max_length=100)
-    town = models.CharField(max_length=40, blank=True)
-    postcode = models.CharField(max_length=20)
-    country = models.CharField(default='UK', max_length=100)
+    name = models.CharField(max_length=50, blank=False)
 
     def __str__(self):
-        return self.last_name
+        return self.name
+
+
+class Address(models.Model):
+    address = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.address
--- api/address_book/serializers.py
+++ api/address_book/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Address
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email',
-                  'street', 'town', 'postcode', 'country')
-        read_only_fields = ['id', 'first_name', 'last_name', 'email']
+        fields = ['id', 'name']
 
+
+class AddressSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Address
+        fields = ['id', 'address', 'user']
